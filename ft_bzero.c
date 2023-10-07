d87e7d1b46ce4351d79073b45907173416a39d42
Martib01 <martybb01@gmail.com>
2023-10-08 00:28:37 +0200

    bzero done

---
+++ ft_bzero.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+/* setting even just the first char in the array to 0 results in an empty string, regardless of what comes after. */
+
+void ft_bzero(void *s, size_t n)
+{
+    ft_memset(s, '\0', n);
+}
+
+int main()
+{
+    char str[] = "Spiderman";
+    size_t n = 2;
+
+    printf("str before bzero: %s\n", str);
+    ft_bzero(str, n);
+    printf("str after bzero: %s\n", str);
+    return 0;
+}
