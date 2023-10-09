3090d03cd59ccf795a69f78f9f0dde0b0e79de56
Martib01 <martybb01@gmail.com>
2023-10-09 23:54:51 +0200

    fix norm

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,19 +1,21 @@
 #include "libft.h"
 
-/* setting even just the first char in the array to 0 results in an empty string, regardless of what comes after. */
-
-void ft_bzero(void *s, size_t n)
+/* setting even just the first char in the array
+	to 0 results in an empty string,
+	regardless of what comes after. */
+void	ft_bzero(void *s, size_t n)
 {
-    ft_memset(s, '\0', n);
+	ft_memset(s, '\0', n);
 }
 
-int main()
-{
-    char str[] = "Spiderman";
-    size_t n = 2;
+// int	main(void)
+// {
+// 	char	str[] = "Spiderman";
+// 	size_t	n;
 
-    printf("str before bzero: %s\n", str);
-    ft_bzero(str, n);
-    printf("str after bzero: %s\n", str);
-    return 0;
-}
+// 	n = 2;
+// 	printf("str before bzero: %s\n", str);
+// 	ft_bzero(str, n);
+// 	printf("str after bzero: %s\n", str);
+// 	return (0);
+// }
