1cc4e32beeb683cd67e7528f42267f4ebddd6b64
Martib01 <martybb01@gmail.com>
2023-10-10 21:31:57 +0200

    putchar_fd done

--- libft.h
+++ libft.h
@@ -32,5 +32,6 @@ char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char *ft_strtrim(char const *s1, char const *set);
+void ft_putchar_fd(char c, int fd);
 
 #endif
