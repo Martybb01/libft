e1133e0a7a6eb81f805e5cefcab2fd8436dd9028
Martib01 <martybb01@gmail.com>
2023-10-10 13:01:06 +0200

    fix norm

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,34 +1,35 @@
 #include "libft.h"
 
-/* confronta il numero specificato di byte del suo primo argomento con i byte corrispondenti del suo secondo argomento.
-Restituisce un valore > 0 se il primo argomento è > del secondo, 0 se sono uguale e un valore < 0 se il primo argomento è < del secondo. */
+/* confronta il numero specificato di byte del suo
+primo argomento con i byte corrispondenti del suo secondo argomento.
+Restituisce un valore > 0 se il primo argomento è > del secondo,
+	0 se sono uguale e un valore < 0 se il primo argomento è < del secondo. */
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    unsigned char *str1;
-    unsigned char *str2;
-    size_t i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
 
-    str1 = (unsigned char *)s1;
-    str2 = (unsigned char *)s2;
-    i = 0;
-
-    while (i < n)
-    {
-        if (str1[i] != str2[i])
-            return (str1[i] - str2[i]);
-        i++;
-    }
-    return (0);
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
 }
 
-int main()
-{
-    char str1[] = "ABCDEFG";
-    char str2[] = "ABCDXYZ";
+// int main()
+// {
+//     char str1[] = "ABCDEFG";
+//     char str2[] = "ABCDXYZ";
 
-    printf("%d\n", ft_memcmp(str1, str2, 4));
-    printf("%d\n", ft_memcmp(str1, str2, 7));
-    printf("%d\n", ft_memcmp(str2, str1, 7));
-    return 0;
-}
+//     printf("%d\n", ft_memcmp(str1, str2, 4));
+//     printf("%d\n", ft_memcmp(str1, str2, 7));
+//     printf("%d\n", ft_memcmp(str2, str1, 7));
+//     return (0);
+// }
