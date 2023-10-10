b0418eddf320c9fc4016f8ee9a3deb612c667ea0
Martib01 <martybb01@gmail.com>
2023-10-10 21:36:41 +0200

    futendl_fd and putstr_fd done

--- libft.h
+++ libft.h
@@ -33,5 +33,7 @@ char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char *ft_strtrim(char const *s1, char const *set);
 void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
 
 #endif
