4c555d4d8579e5fe136f9c677a1af14f4ae85438
Martib01 <martybb01@gmail.com>
2023-10-11 19:54:34 +0200

    isalpha ok

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,9 +1,18 @@
 #include "libft.h"
 
-int ft_isalpha(int c)
+// #include <ctype.h>
+
+int	ft_isalpha(int c)
 {
-    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-        return (1);
-    else
-        return (0);
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
 }
+// int main()
+// {
+//     char c = 'a';
+//     printf("%d\n", ft_isalpha(c));
+//     printf("%d\n", isalpha(c));
+//     return (0);
+// }
