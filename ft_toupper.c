a2d7cd493522da4c5b2b5e80b35bc125978d7c25
Martib01 <martybb01@gmail.com>
2023-10-11 20:28:25 +0200

    toupper ok

--- ft_toupper.c
+++ ft_toupper.c
@@ -1,9 +1,19 @@
 #include "libft.h"
 
-int ft_toupper(int c)
+// #include <ctype.h>
+
+int	ft_toupper(int c)
 {
-    if ((c >= 'a' && c <= 'z'))
-        return (c - 32);
-    else
-        return (c);
+	if ((c >= 'a' && c <= 'z'))
+		return (c - 32);
+	else
+		return (c);
 }
+
+// int main()
+// {
+//     char c = 'm';
+//     printf("%c\n", ft_toupper(c));
+//     printf("%c\n", toupper(c));
+//     return (0);
+// }
