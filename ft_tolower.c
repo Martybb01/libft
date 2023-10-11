7e05c012243d605e8ad14b34ee31dff34fd7b39a
Martib01 <martybb01@gmail.com>
2023-10-11 20:29:51 +0200

    tolower ok

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,9 +1,19 @@
 #include "libft.h"
 
-int ft_tolower(int c)
+// #include <ctype.h>
+
+int	ft_tolower(int c)
 {
-    if ((c >= 'A' && c <= 'Z'))
-        return (c + 32);
-    else
-        return (c);
+	if ((c >= 'A' && c <= 'Z'))
+		return (c + 32);
+	else
+		return (c);
 }
+
+// int main()
+// {
+//     char c = 'M';
+//     printf("%c\n", ft_tolower(c));
+//     printf("%c\n", tolower(c));
+//     return (0);
+// }
