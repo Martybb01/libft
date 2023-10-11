9c974df8090346e5c6502a5d29dfd08fab7583f8
Martib01 <martybb01@gmail.com>
2023-10-11 20:22:58 +0200

    isascii ok

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,9 +1,19 @@
 #include "libft.h"
 
-int ft_isascii(int c)
+// #include <ctype.h>
+
+int	ft_isascii(int c)
 {
-    if ((c >= 0 && c <= 127))
-        return (1);
-    else
-        return (0);
+	if ((c >= 0 && c <= 127))
+		return (1);
+	else
+		return (0);
 }
+
+// int main()
+// {
+//     char c = 'c';
+//     printf("%d\n", ft_isascii(c));
+//     printf("%d\n", isascii(c));
+//     return (0);
+// }
