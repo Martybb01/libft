200a42c7abc1384c08843250c87445119f8b3ed6
Martib01 <martybb01@gmail.com>
2023-10-11 20:10:25 +0200

    isdigit ok

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,9 +1,18 @@
 #include "libft.h"
 
-int ft_isdigit(int c)
+// #include <ctype.h>
+
+int	ft_isdigit(int c)
 {
-    if ((c >= '0' && c <= '9'))
-        return (1);
-    else
-        return (0);
+	if ((c >= '0' && c <= '9'))
+		return (1);
+	else
+		return (0);
 }
+// int main()
+// {
+//     char c = '4';
+//     printf("%d\n", ft_isdigit(c));
+//     printf("%d\n", isdigit(c));
+//     return (0);
+// }
