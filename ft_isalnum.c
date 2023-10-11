dac0dbcdb5fdc37b7226dfe804ff55b520d7219e
Martib01 <martybb01@gmail.com>
2023-10-11 20:16:47 +0200

    isalnum ok

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,9 +1,18 @@
 #include "libft.h"
 
-int ft_isalnum(int c)
+// #include <ctype.h>
+
+int	ft_isalnum(int c)
 {
-    if (ft_isalpha(c) || ft_isdigit(c))
-        return (1);
-    else
-        return (0);
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (1);
+	else
+		return (0);
 }
+// int main()
+// {
+//     char c = 'f';
+//     printf("%d\n", ft_isalnum(c));
+//     printf("%d\n", isalnum(c));
+//     return (0);
+// }
