9b3c37c3a9d67dfc9ae987fbac28dc5693b67b1b
Martib01 <martybb01@gmail.com>
2023-10-11 20:19:11 +0200

    calloc ok

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,12 +1,23 @@
 #include "libft.h"
 
-void *ft_calloc(size_t nitems, size_t size)
+void	*ft_calloc(size_t nitems, size_t size)
 {
-    void *ptr;
+	void	*ptr;
 
-    ptr = (void *)malloc(nitems * size);
-    if (ptr == NULL)
-        return (NULL);
-    ft_bzero(ptr, nitems * size);
-    return (ptr);
+	ptr = (void *)malloc(nitems * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, nitems * size);
+	return (ptr);
 }
+// int main()
+// {
+//     char *str;
+//     char *str2;
+//     str = (char *)ft_calloc(10, sizeof(char));
+//     str2 = (char *)calloc(10, sizeof(char));
+
+//     printf("%s\n", str);
+//     printf("%s\n", str2);
+//     return (0);
+// }
