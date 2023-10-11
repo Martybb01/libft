bb5bedeae13da6046a1b86f6abb2cce35d326616
Martib01 <martybb01@gmail.com>
2023-10-11 10:59:06 +0200

    test ok

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,4 +1,5 @@
 #include "libft.h"
+#include <string.h>
 
 int ft_strncmp(char *s1, char *s2, size_t n)
 {
@@ -13,3 +14,32 @@ int ft_strncmp(char *s1, char *s2, size_t n)
     else
         return (0);
 }
+
+// int main()
+// {
+//     char *s1 = "This is a test string.";
+//     char *s2 = "This is a different test string.";
+//     size_t n = 10;
+
+//     int result = ft_strncmp(s1, s2, n);
+//     int result2 = strncmp(s1, s2, n);
+
+//     if (result == 0 && result2 == 0)
+//     {
+//         printf("The strings are equal for the first %zu characters.\n", n);
+//     }
+//     else if (result < 0 && result2 < 0)
+//     {
+//         printf("The first string is less than the second string for the first %zu characters.\n", n);
+//     }
+//     else if (result > 0 && result2 > 0)
+//     {
+//         printf("The first string is greater than the second string for the first %zu characters.\n", n);
+//     }
+//     else
+//     {
+//         printf("The strings are not equal for the first %zu characters.\n", n);
+//     }
+
+//     return 0;
+// }
