f577d3adbff7bd50508a48cb7b006bc798884e80
Martib01 <martybb01@gmail.com>
2023-10-11 23:18:59 +0200

    strlen ok

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,11 +1,20 @@
 #include "libft.h"
 
-int ft_strlen(const char *s)
-{
-    int i;
+// #include <string.h>
 
-    i = 0;
-    while (s[i] != '\0')
-        i++;
-    return (i);
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
+// int main()
+// {
+// 	char *str = "Hello World!";
+// 	printf("%d\n", ft_strlen(str));
+// 	printf("%lu\n", strlen(str));
+// 	return (0);
+// }
