fad8ad51a83c1509a80e24c9f26ef04027ff9bf5
Martib01 <martybb01@gmail.com>
2023-10-11 20:25:41 +0200

    isprint ok

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,9 +1,18 @@
 #include "libft.h"
 
-int ft_isprint(int c)
+// #include <ctype.h>
+
+int	ft_isprint(int c)
 {
-    if ((c >= 32 && c <= 126))
-        return (1);
-    else
-        return (0);
+	if ((c >= 32 && c <= 126))
+		return (1);
+	else
+		return (0);
 }
+// int main()
+// {
+//     char c = 'c';
+//     printf("%d\n", ft_isprint(c));
+//     printf("%d\n", isprint(c));
+//     return (0);
+// }
