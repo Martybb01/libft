39312c928c0c5e7b8a9e34bf92a2158b99150991
Martib01 <martybb01@gmail.com>
2023-10-12 14:09:54 +0200

    strlcpy done

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,5 +1,7 @@
 #include "libft.h"
 
+// #include <string.h>
+
 unsigned int	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	unsigned int	i;
@@ -21,11 +23,16 @@ unsigned int	ft_strlcpy(char *dest, const char *src, size_t size)
 
 // int main()
 // {
-//     char src[] = "Hello, world!";
-//     char dest[10];
-//     unsigned int len = ft_strlcpy(dest, src, 6);
+// 	char src[] = "Hello, world!";
+// 	char dest[10];
+// 	char src2[] = "Hello, world!";
+// 	char dest2[10];
+// 	unsigned int len = ft_strlcpy(dest, src, 6);
+// 	unsigned int len2 = strlcpy(dest2, src2, 6);
 
-//     printf("String dest is: %s", dest);    // String dest is: Hello
-//     printf("Length of src is: %d\n", len); // Length of src is: 13
-//     return (0);
+// 	printf("String dest is: %s\n", dest);  // String dest is: Hello
+// 	printf("Length of src is: %d\n", len); // Length of src is: 13
+// 	printf("String dest is: %s\n", dest2);
+// 	printf("Length of src is: %d\n", len2);
+// 	return (0);
 // }
