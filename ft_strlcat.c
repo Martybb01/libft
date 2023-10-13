d8e79f4e1e47547084e6cbcba6f21c535cfd09ca
Martib01 <martybb01@gmail.com>
2023-10-13 09:51:00 +0200

    strlcat ok

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -22,3 +22,20 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	dest[dest_len] = '\0';
 	return (src_len);
 }
+// int	main(void)
+// {
+// 	char			src[] = "Hello, world!";
+// 	char			dest[10];
+// 	char			src2[] = "Hello, world!";
+// 	char			dest2[10];
+// 	unsigned int	len;
+// 	unsigned int	len2;
+
+// 	len = ft_strlcat(dest, src, 6);
+// 	len2 = strlcat(dest2, src2, 6);
+// 	printf("String dest is: %s\n", dest);  // String dest is: Hello
+// 	printf("Length of src is: %d\n", len); // Length of src is: 13
+// 	printf("String dest is: %s\n", dest2);
+// 	printf("Length of src is: %d\n", len2);
+// 	return (0);
+// }
