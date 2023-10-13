97ad65daaa6bf636a0cae0a7ccfb867fbd4d668d
Martib01 <martybb01@gmail.com>
2023-10-13 18:13:50 +0200

    memmove ok

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,42 +1,46 @@
 #include "libft.h"
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+// #include <string.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    size_t i;
-    unsigned char *dest2;
-    unsigned char *src2;
+	size_t			i;
+	unsigned char	*dest2;
+	unsigned char	*src2;
 
-    i = 0;
-    dest2 = (unsigned char *)dest;
-    src2 = (unsigned char *)src;
-
-    if (src < dest)
-    {
-        i = n;
-        while (i > 0)
-        {
-            i--;
-            dest2[i] = src2[i];
-        }
-    }
-    else
-    {
-        i = 0;
-        ft_memcpy(dest2, src2, n);
-    }
-    return (dest);
+	i = 0;
+	dest2 = (unsigned char *)dest;
+	src2 = (unsigned char *)src;
+	if (src < dest)
+	{
+		i = n;
+		while (i > 0)
+		{
+			i--;
+			dest2[i] = src2[i];
+		}
+	}
+	else
+	{
+		i = 0;
+		ft_memcpy(dest2, src2, n);
+	}
+	return (dest);
 }
 
 // int main()
 // {
 //     char src[] = "Spiderman";
 //     char dest[] = "boh";
+//     char src2[] = "Spiderman";
+//     char dest2[] = "boh";
 //     size_t n = 3;
 
-//     printf("src before memmove: %s\n", src);
-//     printf("dest before memmove: %s\n", dest);
 //     ft_memmove(dest, src, n);
+//     memmove(dest2, src2, n);
 //     printf("src after memmove: %s\n", src);
 //     printf("dest after memmove: %s\n", dest);
-//     return 0;
+//     printf("src after memmove: %s\n", src2);
+//     printf("dest after memmove: %s\n", dest2);
+//     return (0);
 // }
