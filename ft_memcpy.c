fb5a4b5083e5e19d46be2b512e3fb0b69e7ac3d0
Martib01 <martybb01@gmail.com>
2023-10-13 18:09:25 +0200

    memcpy ok

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,33 +1,37 @@
 #include "libft.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+// #include <string.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-    size_t i;
-    unsigned char *dest2;
-    unsigned char *src2;
+	size_t			i;
+	unsigned char	*dest2;
+	unsigned char	*src2;
 
-    i = 0;
-    dest2 = (unsigned char *)dest;
-    src2 = (unsigned char *)src;
-
-    while (i < n)
-    {
-        dest2[i] = src2[i];
-        i++;
-    }
-    return (dest);
+	i = 0;
+	dest2 = (unsigned char *)dest;
+	src2 = (unsigned char *)src;
+	while (i < n)
+	{
+		dest2[i] = src2[i];
+		i++;
+	}
+	return (dest);
 }
 
 // int main()
 // {
 //     char src[] = "bohfjeifueg";
 //     char dest[] = "spiderman";
+//     char src2[] = "bohfjeifueg";
+//     char dest2[] = "spiderman";
 //     size_t n = 1;
 
-//     printf("src before memcpy: %s\n", src);
-//     printf("dest before memcpy: %s\n", dest);
 //     ft_memcpy(dest, src, n);
+//     memcpy(dest2, src2, n);
 //     printf("src after memcpy: %s\n", src);
 //     printf("dest after memcpy: %s\n", dest);
-//     return 0;
+//     printf("src after memcpy: %s\n", src2);
+//     printf("dest after memcpy: %s\n", dest2);
+//     return (0);
 // }
