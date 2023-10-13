a439b7e76756ecb051d0ea3488921a3e81b6c90c
Martib01 <martybb01@gmail.com>
2023-10-13 18:17:42 +0200

    memchr ok

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,20 +1,21 @@
 #include "libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+// #include <string.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-    size_t i;
-    char *str;
+	size_t	i;
+	char	*str;
 
-    i = 0;
-    str = (char *)s;
-
-    while (i < n)
-    {
-        if ((unsigned char)str[i] == (unsigned char)c)
-            return (str + i); // str + i is the address of the first occurence of c
-        i++;
-    }
-    return (NULL);
+	i = 0;
+	str = (char *)s;
+	while (i < n)
+	{
+		if ((unsigned char)str[i] == (unsigned char)c)
+			return (str + i);
+		i++;
+	}
+	return (NULL);
 }
 
 // int main()
@@ -22,9 +23,12 @@ void *ft_memchr(const void *s, int c, size_t n)
 //     char str[] = "Hello World!";
 //     char c = 'o';
 //     char *p;
+//     char *p2;
 
 //     p = ft_memchr(str, c, 5);
+//     p2 = memchr(str, c, 5);
 
 //     printf("%s", p);
-//     return 0;
+//     printf("%s", p2);
+//     return (0);
 // }
