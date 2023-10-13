109c9dcbd65ca5b0563527b4d2f54a71910c6fad
Martib01 <martybb01@gmail.com>
2023-10-13 18:31:12 +0200

    strdup ok

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,34 +1,37 @@
 #include "libft.h"
 
-char *ft_strdup(char *src)
+// #include <string.h>
+
+char	*ft_strdup(char *src)
 {
-    char *dest;
-    int i;
-    int len;
+	char	*dest;
+	int		i;
+	int		len;
 
-    len = ft_strlen(src);
-    dest = (char *)malloc(sizeof(*dest) * (len + 1));
-
-    if (!dest)
-        return (NULL);
-
-    i = 0;
-    while (src[i])
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	len = ft_strlen(src);
+	dest = (char *)malloc(sizeof(*dest) * (len + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
 // int main()
 // {
 //     char src[] = "fjrgrbrfnowp";
 //     char *dest;
+//     char *dest2;
 
 //     dest = ft_strdup(src);
-//     printf("src: %s\n", src);   // ciao
-//     printf("dest: %s\n", dest); // ciao
-//     return 0;
+//     dest2 = strdup(src);
+//     printf("src: %s\n", src);
+//     printf("dest: %s\n", dest);
+//     printf("dest2: %s\n", dest2);
+//     return (0);
 // }
