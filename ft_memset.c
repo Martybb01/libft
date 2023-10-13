22a84b6b6b482d81f303e80c538d7f906cc4a044
Martib01 <martybb01@gmail.com>
2023-10-13 17:14:28 +0200

    memset ok

--- ft_memset.c
+++ ft_memset.c
@@ -1,30 +1,35 @@
 #include "libft.h"
 
-void *ft_memset(void *s, int c, size_t n)
-{
-    size_t i;
-    unsigned char *str;
-    char zero;
+// #include <string.h>
 
-    i = 0;
-    str = (unsigned char *)s;
-    zero = c;
-    while (i < n)
-    {
-        str[i] = zero;
-        i++;
-    }
-    return (str);
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+	char			zero;
+
+	i = 0;
+	str = (unsigned char *)s;
+	zero = c;
+	while (i < n)
+	{
+		str[i] = zero;
+		i++;
+	}
+	return (str);
 }
 
 // int main()
 // {
 //     char str[] = "Hello World!";
-//     char c = 'o';
+//     char c = 'l';
 //     char *p;
+//     char *p2;
 
 //     p = ft_memset(str, c, 5);
+//     p2 = memset(str, c, 5);
 
-//     printf("%s", p);
-//     return 0;
+//     printf("%s\n", p);
+//     printf("%s", p2);
+//     return (0);
 // }
