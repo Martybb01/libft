f4971890b63b8339a8d51809de852714cb01c73c
Martib01 <martybb01@gmail.com>
2023-10-14 14:37:51 +0200

    substr ok

--- ft_substr.c
+++ ft_substr.c
@@ -1,32 +1,29 @@
 #include "libft.h"
 
-/* Allocates (with malloc(3)) and returns a substring from the string ’s’.
-The substring begins at index ’start’ and is of maximum size ’len’. */
-
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char *str;
+	char	*str;
 
-    if (!s)
-        return (NULL);
-    if ((int)start > ft_strlen(s))
-        return (ft_strdup(""));
-    if ((int)len > ft_strlen(s + start))
-        len = ft_strlen(s + start);
-    str = (char *)malloc(sizeof(*str) * (len + 1));
-    if (!str)
-        return (NULL);
-    ft_memcpy(str, s + start, len);
-    str[len] = '\0';
-    return (str);
+	if (!s)
+		return (NULL);
+	if ((int)start > ft_strlen(s))
+		return (ft_strdup(""));
+	if ((int)len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	str = (char *)malloc(sizeof(*str) * (len + 1));
+	if (!str)
+		return (NULL);
+	ft_memcpy(str, s + start, len);
+	str[len] = '\0';
+	return (str);
 }
 
 // int main()
 // {
-//     char s[] = "Hello World!";
+//     char s[] = "Hello";
 //     char *p;
 
-//     p = ft_substr(s, 0, 3);
+//     p = ft_substr(s, 3, 9);
 //     printf("%s", p);
-//     return 0;
+//     return (0);
 // }
